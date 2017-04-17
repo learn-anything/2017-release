a8723eb7f5f8a706de236e42f4873a1a9e8cae99
Nikita Voloboev <nikita.voloboev@gmail.com>
2017-04-17 12:28:18 +0200

    prefixes explanation for searching

--- js/index.js
+++ js/index.js
@@ -7,6 +7,12 @@ function presentMindMap(query) {
 
 // TODO: for now just to get something working, make a switch case of all possible mind maps
 
+// NOTE: queries can be prefixed by certain words to scope what the search is about
+// i.e. if query is 'learn machine learning', mind map from 'learn anything' is shown
+// if query is 'learn neural networks', mind map from 'learn anything - machine learning' is shown focused just on the 'neural networks' part
+// other prexises may include 'interesting', i.e. 'interesting blogs on javascript' or 'interesting websites on education'
+// list can go on but that should be taken into account
+
 // TODO: add header text displaying that mind map can be improved if user disagrees with contents of it
 
 // TODO: remove iframe when user starts typing a query again
