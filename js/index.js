a97987bdf971ca1aa8f6c4f741ef46a18ac1f4f3
Nikita Voloboev <nikita.voloboev@gmail.com>
2017-04-17 12:24:09 +0200

    added todos

--- js/index.js
+++ js/index.js
@@ -1,3 +1,12 @@
+// TODO: user writes query, iframe is presented from below switch case, if query is the same
 function presentMindMap(query) {
 
 }
+
+// TODO: add autosuggestions to search bar so users can see what results are available, perhaps there is a library
+
+// TODO: for now just to get something working, make a switch case of all possible mind maps
+
+// TODO: add header text displaying that mind map can be improved if user disagrees with contents of it
+
+// TODO: remove iframe when user starts typing a query again
