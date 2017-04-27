4c6c5a00930f232838f9218ec440cbda819285cd
Nikita Voloboev <nikita.voloboev@gmail.com>
2017-04-27 23:04:57 +0200

    create react app

---
+++ src/Search.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+
+class Search extends Component {
+  render() {
+    return (
+        <h1>Hello</h1>
+    );
+  }
+}
+
+
+
+export default Search;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Search from './Search';
+import './index.css';
+
+ReactDOM.render(
+  <Search />,
+  document.getElementById('root')
+);
