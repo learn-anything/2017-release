fdf1aae49b6c73b4affe5bd524a1fb5053779491
Nikita Voloboev <nikita.voloboev@gmail.com>
2017-05-02 23:47:21 +0200

    :sparkles:

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+)
+
+var assetsPath string
+
+func main() {
+
+
+}
--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Search from './Search';
-import './index.css';
-
-ReactDOM.render(
-  <Search />,
-  document.getElementById('root')
-);
