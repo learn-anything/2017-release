ce072a2d152a297158cdd476691d98996bad6364
Nikita Voloboev <nikita.voloboev@gmail.com>
2017-05-03 13:05:42 +0200

    main

--- main.go
+++ main.go
@@ -1,11 +1,33 @@
 package main
 
 import (
+	"github.com/nikitavoloboev/knowledge-map-search-engine/daemon"
 )
 
 var assetsPath string
 
+func processFlags() *daemon.Config {
+	cfg := &daemon.Config{}
+
+	flag.StringVar(&cfg.ListenSpec, "listen", "localhost:3000", "HTTP listen spec")
+	flag.StringVar(&cfg.Db.ConnectString, "db-connect", "host=/var/run/postgresql dbname=search sslmode=disable", "DB Connect String")
+	flag.StringVar(&assetsPath, "assets-path", "assets", "Path to assets dir")
+
+	flag.Parse()
+	return cfg
+}
+
+func setupHttpAssets(cfg *daemon.Config) {
+	log.Printf("Assets served from %q.", assetsPath)
+	cfg.UI.Assets = http.Dir(assetsPath)
+}
+
 func main() {
+	cfg := processFlags()
 
+	setupHttpAssets(cfg)
 
+	if err := daemon.Run(cfg); err != nil {
+		log.Printf("Error in main(): %v", err)
+	}
 }
