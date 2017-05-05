c88b5f6c0b54c9e8cc6e7193c6e82a9c6d96d35b
Nikita Voloboev <nikita.voloboev@gmail.com>
2017-05-05 21:39:05 +0200

    :sparkles:

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Search from './components/Search';
+import './index.css';
+
+ReactDOM.render(
+  <Search />,
+  document.getElementById('root')
+);
