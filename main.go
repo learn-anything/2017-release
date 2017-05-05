f4dbbc860b21e0d09262a8531106799b56ed7aef
Nikita Voloboev <nikita.voloboev@gmail.com>
2017-05-05 04:42:58 +0200

    :sparkles:

--- main.go
+++ main.go
@@ -5,10 +5,6 @@ import (
 	"net/http"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello world!")
-}
-
 func main() {
 	http.Handle("/", http.FileServer(http.Dir("./static")))
 	http.ListenAndServe(":3000", nil)
