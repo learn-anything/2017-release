18b8dafb5ba299dba842457e9aaae49eb0f88cea
Nikita Voloboev <nikita.voloboev@gmail.com>
2017-05-05 03:31:31 +0200

    testing

--- main.go
+++ main.go
@@ -1,33 +1,15 @@
 package main
 
 import (
-	"github.com/nikitavoloboev/knowledge-map-search-engine/daemon"
+	"fmt"
+	"net/http"
 )
 
-var assetsPath string
-
-func processFlags() *daemon.Config {
-	cfg := &daemon.Config{}
-
-	flag.StringVar(&cfg.ListenSpec, "listen", "localhost:3000", "HTTP listen spec")
-	flag.StringVar(&cfg.Db.ConnectString, "db-connect", "host=/var/run/postgresql dbname=search sslmode=disable", "DB Connect String")
-	flag.StringVar(&assetsPath, "assets-path", "assets", "Path to assets dir")
-
-	flag.Parse()
-	return cfg
-}
-
-func setupHttpAssets(cfg *daemon.Config) {
-	log.Printf("Assets served from %q.", assetsPath)
-	cfg.UI.Assets = http.Dir(assetsPath)
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello world!")
 }
 
 func main() {
-	cfg := processFlags()
-
-	setupHttpAssets(cfg)
-
-	if err := daemon.Run(cfg); err != nil {
-		log.Printf("Error in main(): %v", err)
-	}
+	http.Handle("/", http.FileServer(http.Dir("./static")))
+	http.ListenAndServe(":3000", nil)
 }
