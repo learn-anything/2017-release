055bd4ccac5840ee774694be3b85b107f748941c
Nikita Voloboev <nikita.voloboev@gmail.com>
2017-05-12 16:27:22 +0200

    added web dev

--- client/triggers.js
+++ client/triggers.js
@@ -3,6 +3,5 @@ const triggers = [
   { name: 'mathematics', map: 'JD2DJUeZSAwQzyb5KceAZ6bfuYaLGqsDkpfZg1R2#461.4,-683.1,-1' },
   { name: 'physics', map: 'RfCpjqozA6Aq3j38RvzUnZrhFGMnpCsbRrpAsUWj#-201.2,-115.3,1' },
   { name: 'machine learning', map: 'k954FWFNztnkphWmNqqHz2dqQW41LeP1SLqkqqjs#316.4,-944.7,0' },
-  { name: 'courses on' },
-  { name: 'books on' },
+  { name: 'web development', map: '85uh2iN4GPhBJpuyyuWks5a6niyypQA5TPBCGnAN#521.7,-260.4,2' },
 ];
