d32d9e3c937c5517a736787cff44c5a7794b3d26
nglgzz <angelo@nglgzz.com>
2017-06-04 02:12:52 +0200

    added improve this map button

--- client/components/ContributeButton.jsx
+++ client/components/ContributeButton.jsx
@@ -2,22 +2,37 @@ import { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import '../sass/_ContributeButton.sass';
 
-@connect(store => ({
-  issue: store.map.issueUrl
-}))
-
-
-
+@connect(store => ({ issue: store.map.issueUrl }))
 export default class ContributeButton extends Component {
+  onClick() {
+    setTimeout(() => { location.href = this.props.issue; }, 500);
+
+    ga('send', 'event', {
+      eventCategory: 'Coontribution',
+      eventAction: 'contribution button clicked',
+      eventLabel: this.props.issue,
+      hitCallback: () => { location.href = this.props.issue; },
+    });
+  }
+
   render() {
-    function onClick() {
-     window.location.href = this.props.issue;
+    if (this.props.issue !== '') {
+      return (
+        <button
+          onClick={this.onClick.bind(this)}
+          className="contribute-button"
+          type="button"
+        >
+          <span className="contribute-button-text">Improve this map</span>
+          <img
+            className="contribute-button-emoji"
+            src="https://assets-cdn.github.com/images/icons/emoji/unicode/1f984.png"
+          />
+        </button>
+      );
     }
-    return (
-      <button className="contribute-button" type="button" onClick={this.onClick.bind(this)}>
-        Improve it 🦄
-      </button>
-    );
+
+    return (<div></div>);
   }
 }
 
