83ce7d3885eab81677a2a867b85ef691d0153a21
Nikita Voloboev <nikita.voloboev@gmail.com>
2017-07-17 18:53:14 +0200

    :sparkles:

--- client/components/Sidebar.jsx
+++ client/components/Sidebar.jsx
@@ -25,9 +25,6 @@ export default class Sidebar extends Component {
       case 'night':
         require('../themes/_Night.sass');
         break;
-      case 'grey':
-        require('../themes/_Grey.sass');
-        break;
     }
   }
 
@@ -54,12 +51,16 @@ export default class Sidebar extends Component {
                 Themes 🖌
               </div>
             </h1>
-          {/*<h1>
-              <div id="option-bookmarks" className="menu-item">
-                Bookmarks 🔖
+            <h1>
+              <div
+                id="option-tips"
+                onClick={this.toggleTips.bind(this)}
+                className="menu-item"
+              >
+                Tips 💎
               </div>
             </h1>
-            <h1>
+            {/* <h1>
               <div id="option-learned-maps" className="menu-item">
                 Learned Maps ✅
               </div>
@@ -68,7 +69,53 @@ export default class Sidebar extends Component {
               <div id="option-path-picker" className="menu-item">
                 Path Picker 🗺
               </div>
-            </h1>*/}
+            </h1> */}
+          </Menu>
+        );
+      case 'option-tips':
+        return (
+          <Menu right width={340}>
+            <h1>
+              <div
+                id="option-tips"
+                onClick={this.toggleTips.bind(this)}
+                className="menu-item"
+              >
+                Tips 💎
+              </div>
+            </h1>
+            <div>
+              <div>The path below the search bar shows where you are</div>
+              <div>You can click on that path to go back to different maps</div>
+              <div>
+                Pressing return on the suggestion options in the search bar is a
+                fun way to explore new maps
+              </div>
+              <div>
+                Hovering over a node will display a note if that node has a note
+              </div>
+              <div>Help button is a great way to reference emoji meanings</div>
+              <div>
+                All basics nodes have steps like 1. 2. and so on, if nodes have
+                the same step, just choose any you like
+              </div>
+              <div>
+                Certain nodes have a prefix like 17: or 15: which stands for the
+                year in which that article or video or paper was writen in
+              </div>
+              <div>
+                Nearly all nodes have a reddit subreddit or stack exchange forum
+                attached to them, they're a great place to ask questions you may
+                have
+              </div>
+              <div>
+                You can also join our{' '}
+                <a href="https://knowledge-map.slack.com/shared_invite/MTgxNTYzMjIzNjM5LTE0OTQzMzA4MDAtYzY1YWY0ZDc0NQ">
+                  community on Slack
+                </a>{' '}
+                to learn together and help the project evolve
+              </div>
+            </div>
           </Menu>
         );
       case 'option-themes':
@@ -85,7 +132,7 @@ export default class Sidebar extends Component {
             </h1>
             <h1>
               <div
-                id="grey"
+                id="night"
                 onClick={this.showSettings.bind(this)}
                 className="menu-item"
               >
