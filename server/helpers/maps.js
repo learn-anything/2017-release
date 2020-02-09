462d8a3705f5f58e4eafaef39f82b2b87c78f89d
Angelo Gazzola <angelo@nglgzz.com>
2020-02-09 11:10:30 +0100

    Server: remove elasticsearch suggestions

--- server/helpers/maps.js
+++ server/helpers/maps.js
@@ -1,76 +1,72 @@
-const elastic = require('../utils/elasticClient');
-const { cache } = require('../utils/cache');
-const dynamo = require('../utils/dynamoClient');
-const { cacheKeys } = require('../constants.json');
-const { APIError } = require('../utils/errors');
+const Fuse = require("fuse.js");
+const { cache } = require("../utils/cache");
+const dynamo = require("../utils/dynamoClient");
+const suggestions = require("../data/suggestions.json");
+const { APIError } = require("../utils/errors");
+const { cacheKeys } = require("../constants.json");
 
+const fuse = new Fuse(suggestions, { keys: ["key"] });
+const suggestionsByTitle = suggestions.reduce(
+  (obj, sugg) => ({ ...obj, [sugg.title]: sugg }),
+  {}
+);
 
 // Fuzzy search maps by key name.
 async function fuzzySearch(query) {
   // If the query is specified search for that, otherwise return a
   // random document.
-  const response = await elastic.client.search({
-    index: 'maps',
-    body: query ? elastic.fuzzy('key', query) : elastic.random(),
-  });
+  if (query) {
+    return fuse.search(query);
+  }
 
-  // Format results nicely before returning them.
-  return response.hits.hits.map((hit) => {
-    let title = hit._source.title.replace(/ - /g, '/').replace(/ /g, '-');
-    if (title !== 'learn-anything') {
-      title = title.replace('learn-anything', '');
-    }
-
-    return {
-      title,
-      key: hit._source.key,
-      id: hit._id,
-      // TODO - consider adding number of resources here.
-    };
-  });
+  const randIndex = Math.floor(Math.random() * (suggestions.length - 1));
+  return [suggestions[randIndex]];
 }
 
 // Get a specific map by ID.
 async function byID(mapID) {
   // Get Map metadata from DynamoDB.
-  const { Item } = await dynamo('get', {
-    TableName: 'Maps',
-    Key: { mapID: Number(mapID) },
+  const { Item } = await dynamo("get", {
+    TableName: "Maps",
+    Key: { mapID: Number(mapID) }
   });
 
   const map = {
-      ...Item,
-      nodes: {},
-      resources: {},
+    ...Item,
+    nodes: {},
+    resources: {}
   };
 
   // Query DynamoDB to get the nodes for the current map.
-  const nodesPromise = dynamo('query', {
-    TableName: 'Nodes',
-    IndexName: 'MapIndex',
-    Select: 'ALL_ATTRIBUTES',
-    KeyConditionExpression: 'mapID = :value',
+  const nodesPromise = dynamo("query", {
+    TableName: "Nodes",
+    IndexName: "MapIndex",
+    Select: "ALL_ATTRIBUTES",
+    KeyConditionExpression: "mapID = :value",
     ExpressionAttributeValues: {
-      ':value': Number(mapID),
-    },
+      ":value": Number(mapID)
+    }
   });
 
   // Query DynamoDB to get the resources for the current map.
-  const resourcesPromise = dynamo('query', {
-    TableName: 'Resources',
-    IndexName: 'MapIndex',
-    Select: 'ALL_ATTRIBUTES',
-    KeyConditionExpression: 'mapID = :value',
+  const resourcesPromise = dynamo("query", {
+    TableName: "Resources",
+    IndexName: "MapIndex",
+    Select: "ALL_ATTRIBUTES",
+    KeyConditionExpression: "mapID = :value",
     ExpressionAttributeValues: {
-      ':value': Number(mapID),
-    },
+      ":value": Number(mapID)
+    }
   });
 
-  const [nodes, resources] = await Promise.all([nodesPromise, resourcesPromise]);
+  const [nodes, resources] = await Promise.all([
+    nodesPromise,
+    resourcesPromise
+  ]);
 
   // Convert the list to a dictionary having parent nodes as keys, and lists
   // of nodes as values. This is used by the render component.
-  nodes.Items.forEach((node) => {
+  nodes.Items.forEach(node => {
     if (map.nodes[node.parentID]) {
       // If there's already some nodes with the same parent, append this node
       // to the list.
@@ -91,7 +87,7 @@ async function byID(mapID) {
 
   // Convert the list to a dictionary having parent nodes as keys, and lists
   // of resources as values. This is used by the render component.
-  resources.Items.forEach((resource) => {
+  resources.Items.forEach(resource => {
     // Same logic as above apply, only that we don't have a "root resource".
     // All resources must have a parent node, and no resource has a child.
     if (map.resources[resource.parentID]) {
@@ -106,31 +102,22 @@ async function byID(mapID) {
 
 // Get a specific map by title.
 async function byTitle(title) {
-  // Query elasticsearc for metadata of a map, given its title.
-  const metaByTitle = elastic.client.search({
-    index: 'maps',
-    body: elastic.get({ title }),
-  });
+  const cleanTitle = title
+    .replace(/^learn anything - /, "/")
+    .replace(/ - /g, "/")
+    .replace(/ /g, "-");
+  const { id } = suggestionsByTitle[cleanTitle] || {};
 
-  const key1 = cacheKeys.mapID.byTitle + title.replace(/\s/g, '-');
-  const response = await cache(key1, metaByTitle);
-
-  const hits = response.hits.hits;
-
-  // There can't be more than one result, as the limit for this ES query is 1,
-  // and in any case, map titles should be unique.
-  if (hits.length !== 1) {
-    throw new APIError(404, 'map not found');
+  if (id === undefined) {
+    throw new APIError(404, "map not found");
   }
 
-  // Now that we have the ID, let's retrieve the whole map.
-  const key2 = cacheKeys.maps.byID + hits[0]._id;
-  return cache(key2, byID(hits[0]._id));
+  const key2 = cacheKeys.maps.byID + id;
+  return cache(key2, byID(id));
 }
-
 
 module.exports = {
   fuzzySearch,
   byID,
-  byTitle,
+  byTitle
 };
